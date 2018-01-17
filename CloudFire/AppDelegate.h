76a3e7b8c586b652d3ad1bc2e566258561c125dc
macos <macos@macosdeMac.local>
2018-01-17 17:40:37 +0800

    first commit

---
+++ CloudFire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudFire
+//
+//  Created by macos on 2018/1/17.
+//  Copyright © 2018年 jbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudFire/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudFire
+//
+//  Created by macos on 2018/1/17.
+//  Copyright © 2018年 jbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
