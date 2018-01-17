3ede171c54e8d22ed28956db3deebcc341a0077c
macos <macos@macosdeMac.local>
2018-01-17 17:57:13 +0800

    新增Tab bar

---
+++ CloudFire/CloudTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  CloudTabBarController.h
+//  CloudFire
+//
+//  Created by macos on 2018/1/17.
+//  Copyright © 2018年 jbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CloudTabBarController : UITabBarController
+
+@end
